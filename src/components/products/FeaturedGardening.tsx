bac8784d6d70f2bbf80e750fbd446f9e445ec513
kiko-g <kikojpgoncalves@gmail.com>
2023-01-15 01:36:55 +0000

    update: products main page

--- src/components/products/FeaturedGardening.tsx
+++ src/components/products/FeaturedGardening.tsx
@@ -1,27 +1,30 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 type Props = {}
 
 export default function FeaturedGardening({}: Props) {
+  const title = `Jardinagem`
+  const text = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore officia corporis quasi doloribus iure architecto quae voluptatum beatae excepturi dolores.`
+
   return (
-    <section className="overflow-hidden bg-[url(https://images.unsplash.com/photo-1602810319428-019690571b5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
-      <div className="bg-black/25 p-8 md:p-12 lg:px-16 lg:py-24">
-        <div className="max-w-lg text-center sm:text-left">
-          <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo!
-          </h2>
-
-          <p className="hidden max-w-md text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore officia corporis quasi doloribus iure
-            architecto quae voluptatum beatae excepturi dolores.
+    <section className="w-full overflow-hidden rounded bg-[url(https://images.unsplash.com/photo-1602810319428-019690571b5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
+      <div className="bg-gradient-to-l from-black/60 to-black/20 p-8 md:p-12 lg:px-16 lg:py-24">
+        <div className="flex flex-col items-center text-center sm:text-right md:items-end">
+          <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl">{title}</h2>
+          <p
+            className="max-w-md text-sm font-normal leading-tight text-white/90 
+            md:mt-6 md:block md:text-lg md:leading-relaxed"
+          >
+            {text}
           </p>
-
           <div className="mt-4 sm:mt-8">
-            <a
-              href="#"
-              className="inline-flex items-center rounded-full bg-indigo-700 px-8 py-3 text-white shadow-lg transition hover:bg-indigo-600 focus:outline-none focus:ring"
+            <Link
+              to="jardinagem"
+              className="inline-flex items-center rounded-full bg-primary px-8 py-3 text-white shadow-lg 
+              transition hover:opacity-80 focus:outline-none focus:ring dark:bg-secondary"
             >
-              <span className="text-sm font-medium"> Order Now </span>
+              <span className="text-sm font-medium">Ver produtos</span>
 
               <svg
                 className="ml-3 h-5 w-5"
@@ -30,9 +33,9 @@ export default function FeaturedGardening({}: Props) {
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
               </svg>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
