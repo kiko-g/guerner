5fd1a58be4a8497c197ff108961c545a6fff1fec
kiko-g <kikojpgoncalves@gmail.com>
2023-01-23 20:27:29 +0000

    update: footer

--- src/config/routing.ts
+++ src/config/routing.ts
@@ -6,6 +6,20 @@ interface Routes {
     main: string
     agriculture: string
     construction: string
+    agricultureMd?: {
+      produto1: string
+      produto2: string
+      produto3: string
+      produto4: string
+      produto5: string
+    }
+    constructionMd?: {
+      produto1: string
+      produto2: string
+      produto3: string
+      produto4: string
+      produto5: string
+    }
   }
   info: {
     privacyPolicy: string
@@ -20,6 +34,20 @@ const pt: Routes = {
     main: '/pt/produtos/',
     agriculture: '/pt/produtos/agricultura/',
     construction: '/pt/produtos/construcao/',
+    agricultureMd: {
+      produto1: '/pt/produtos/agricultura/produto1',
+      produto2: '/pt/produtos/agricultura/produto2',
+      produto3: '/pt/produtos/agricultura/produto3',
+      produto4: '/pt/produtos/agricultura/produto4',
+      produto5: '/pt/produtos/agricultura/produto5',
+    },
+    constructionMd: {
+      produto1: '/pt/produtos/construcao/produto1',
+      produto2: '/pt/produtos/construcao/produto2',
+      produto3: '/pt/produtos/construcao/produto3',
+      produto4: '/pt/produtos/construcao/produto4',
+      produto5: '/pt/produtos/construcao/produto5',
+    },
   },
   contacts: '/pt/contactos/',
   info: {
@@ -34,7 +62,21 @@ const en: Routes = {
   products: {
     main: '/en/products/',
     agriculture: '/en/products/agriculture/',
-    construction: '/pt/products/construction/',
+    construction: '/en/products/construction/',
+    agricultureMd: {
+      produto1: '/en/products/agriculture/produto1',
+      produto2: '/en/products/agriculture/produto2',
+      produto3: '/en/products/agriculture/produto3',
+      produto4: '/en/products/agriculture/produto4',
+      produto5: '/en/products/agriculture/produto5',
+    },
+    constructionMd: {
+      produto1: '/en/products/construction/produto1',
+      produto2: '/en/products/construction/produto2',
+      produto3: '/en/products/construction/produto3',
+      produto4: '/en/products/construction/produto4',
+      produto5: '/en/products/construction/produto5',
+    },
   },
   contacts: '/pt/contacts/',
   info: {
@@ -44,8 +86,8 @@ const en: Routes = {
 }
 
 const routes = {
-  pt: pt,
-  en: en,
+  pt,
+  en,
 }
 
 export default routes
