ce31d3c0ae56aea01c647601fd61947cf4c9ca60
kiko-g <kikojpgoncalves@gmail.com>
2023-06-16 18:20:24 +0100

    fix: style change in products page

--- src/components/products/FeaturedAgriculture.tsx
+++ src/components/products/FeaturedAgriculture.tsx
@@ -21,11 +21,11 @@ export default function FeaturedAgriculture({}: Props) {
         className="absolute left-0 top-0 z-[-1] h-full w-full object-cover"
       />
       <div className="bg-gradient-to-r from-black/75 via-black/50 to-black/25 p-8 md:p-12 lg:px-16 lg:py-24">
-        <div className="flex flex-col items-center text-center sm:text-left md:items-start">
+        <div className="flex flex-col items-center text-center md:items-start md:text-left">
           <h2 className="mb-2 text-2xl font-bold text-white sm:text-3xl md:mb-1 md:text-5xl">
             {header}
           </h2>
-          <p className="max-w-sm text-sm font-normal leading-tight text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed">
+          <p className="max-w-sm text-center text-sm font-normal leading-tight text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed lg:text-left">
             {description}
           </p>
           <div className="mt-4 sm:mt-8">
