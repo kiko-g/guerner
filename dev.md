b2f571343bd0cf1bf149cf6b96cc867b5398edc3
kiko-g <kikojpgoncalves@gmail.com>
2023-12-07 20:17:10 +0000

    add: enhance images script and instructions

--- dev.md
+++ dev.md
@@ -1,9 +1,17 @@
 # Development Notes
 
-## Content Management
+## Check Image Content
 
 To see whether the file content for the products is matching the expected format, run the following command:
 
 ```bash
-python scripts/content.py src/markdown
+python scripts/check-content.py src/markdown
+```
+
+## Enhance Images
+
+To prevent low quality, blurry images falling on our website we can enhance them with the following command:
+
+```bash
+python scripts/enhance-images.py src/markdown
 ```
---
+++ scripts/enhance-images.py
@@ -0,0 +1,40 @@
+import os
+import sys
+from PIL import Image, ImageFilter
+
+
+def enhance_image(image_path):
+    with Image.open(image_path) as img:
+        width, height = img.size
+
+        if width < 512 or height < 512:
+            ratio = max(512 / width, 512 / height)
+            new_size = (int(width * ratio), int(height * ratio))
+            # Resize the image with ANTIALIAS filter to reduce jaggies
+            enhanced_img = img.resize(new_size, Image.ANTIALIAS)
+            # Apply unsharp mask to the resized image
+            enhanced_img = enhanced_img.filter(ImageFilter.UnsharpMask(radius=2, percent=150, threshold=3))
+            enhanced_img.save(image_path)
+            print(f"Enhanced image saved: {image_path}")
+
+
+def process_images(directory):
+    for subdir, dirs, files in os.walk(directory):
+        for file in files:
+            if file.lower().endswith((".png", ".jpg", ".jpeg", ".webp")):
+                file_path = os.path.join(subdir, file)
+                enhance_image(file_path)
+
+
+def main(directory):
+    process_images(directory)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <path_to_base_directory>")
+        print("Example: python scripts/enhance-images.py src/markdown")
+        sys.exit(1)
+
+    directory = sys.argv[1]
+    main(directory)
