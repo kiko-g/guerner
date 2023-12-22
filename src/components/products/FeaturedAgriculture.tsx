7cf4b368231f7b7e834f40b1b5146f257f5047df
kiko-g <kikojpgoncalves@gmail.com>
2023-12-22 01:10:27 +0000

    fix: agriculture banner in production

--- src/components/products/FeaturedAgriculture.tsx
+++ src/components/products/FeaturedAgriculture.tsx
@@ -14,24 +14,22 @@ export default function FeaturedAgriculture({}: Props) {
   const takeMeThere = t('agricultureTakeMeThere')
 
   return (
-    <section className="relative w-full">
+    <section className="relative w-full bg-gradient-to-r from-black/60 via-black/50 to-black/40 p-8 lg:p-12 lg:px-16 lg:py-24">
       <StaticImage
         src="../../images/agriculture.jpg"
         alt="Agriculture @ Guerner"
         placeholder="blurred"
         className="absolute left-0 top-0 z-[-1] h-full w-full object-cover"
       />
-      <div className="bg-gradient-to-r from-black/60 via-black/50 to-black/40 p-8 lg:p-12 lg:px-16 lg:py-24">
-        <div className="flex flex-col items-center text-center lg:items-start lg:text-left">
-          <h2 className="mb-2 text-2xl font-bold text-white sm:text-3xl lg:mb-1 lg:text-5xl">
-            {header}
-          </h2>
-          <p className="max-w-sm text-center text-sm font-normal leading-tight text-white/90 lg:mt-6 lg:block lg:text-left lg:text-lg lg:leading-relaxed">
-            {description}
-          </p>
-          <div className="mt-4 sm:mt-8">
-            <LinkFill link={route} text={takeMeThere} light />
-          </div>
+      <div className="flex flex-col items-center text-center lg:items-start lg:text-left">
+        <h2 className="mb-2 text-2xl font-bold text-white sm:text-3xl lg:mb-1 lg:text-5xl">
+          {header}
+        </h2>
+        <p className="max-w-sm text-center text-sm font-normal leading-tight text-white/90 lg:mt-6 lg:block lg:text-left lg:text-lg lg:leading-relaxed">
+          {description}
+        </p>
+        <div className="mt-4 sm:mt-8">
+          <LinkFill link={route} text={takeMeThere} light />
         </div>
       </div>
     </section>
