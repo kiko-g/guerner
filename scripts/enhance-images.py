9f615725770f569fcea070afb378401ce94fdf66
kiko-g <kikojpgoncalves@gmail.com>
2023-12-22 00:28:51 +0000

    update: enhance some images

--- scripts/enhance-images.py
+++ scripts/enhance-images.py
@@ -1,21 +1,29 @@
 import os
 import sys
-from PIL import Image, ImageFilter
+from PIL import Image, ImageEnhance, ImageFilter
 
 
 def enhance_image(image_path):
-    width_threshold = 512
-    height_threshold = 512
+    width_threshold = 416
+    height_threshold = 416
     with Image.open(image_path) as img:
         width, height = img.size
 
         if width < width_threshold or height < height_threshold:
             ratio = max(width_threshold / width, height_threshold / height)
             new_size = (int(width * ratio), int(height * ratio))
-            # Resize the image with ANTIALIAS filter to reduce jaggies
-            enhanced_img = img.resize(new_size, Image.ANTIALIAS)
-            # Apply unsharp mask to the resized image
-            enhanced_img = enhanced_img.filter(ImageFilter.UnsharpMask(radius=2, percent=150, threshold=3))
+
+            # Resize the image with LANCZOS filter (formerly ANTIALIAS) to reduce jaggies
+            enhanced_img = img.resize(new_size, Image.LANCZOS)
+
+            # Apply a milder unsharp mask to the resized image
+            enhanced_img = enhanced_img.filter(ImageFilter.UnsharpMask(radius=1, percent=100, threshold=2))
+
+            # Increase the contrast slightly
+            enhancer = ImageEnhance.Contrast(enhanced_img)
+            enhanced_img = enhancer.enhance(1.1)
+
+            # Save the enhanced image
             enhanced_img.save(image_path)
             print(f"Enhanced image saved: {image_path}")
 
@@ -35,7 +43,6 @@ def main(directory):
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py <path_to_base_directory>")
-        print("Example: python scripts/enhance-images.py src/markdown")
         sys.exit(1)
 
     directory = sys.argv[1]
